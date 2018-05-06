da1cb83cd05bd8409ab8e854fabc42cbeb6ca9c8
ioana <geantaioana13@stud.ase.ro>
2018-05-06 16:45:25 +0300

    first commit

---
+++ app/src/main/java/com/ase/simpre/webstudentbot2018/FirebaseApp.java
@@ -0,0 +1,12 @@
+package com.ase.simpre.webstudentbot2018;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseApp extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
